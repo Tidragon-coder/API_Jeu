01da846b6aa332a72a455320ba39dc6c636b52f9
Tidragon <theomoon77@gmail.com>
2025-11-24 23:13:21 +0100

    #042 / refacto : Add real data to the user graph in the dashboard
    - getUserStatsPerDay
    - getDateWeeksAgo

--- admin-dashboard/src/components/molecules/GraphDashboard.tsx
+++ admin-dashboard/src/components/molecules/GraphDashboard.tsx
@@ -1,28 +1,63 @@
+import { useEffect, useState } from "react";
 import Box from '@mui/material/Box';
+import axios from "axios";
 import { LineChart } from '@mui/x-charts/LineChart';
+import callApi from "../../api/api";
+import Error from "../../components/molecules/Error";
+import type { ErrorState } from '../../types/error';
 
 const margin = { right: 24 };
-const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
-const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
-const xLabels = [
-    'Page A',
-    'Page B',
-    'Page C',
-    'Page D',
-    'Page E',
-    'Page F',
-    'Page G',
+const Time = [
+    '5 sem',
+    '4 sem',
+    '3 sem',
+    '2 sem',
+    '1 sem',
+    'Today',
 ];
 
+
 export default function SimpleLineChart() {
+    const [error, setError] = useState<ErrorState>({ code: 0, message: "" });
+    const [userData, setUserDate] = useState<number[]>([]);
+    
+    const fetchData = async () => {
+        try {
+            const token = localStorage.getItem("token");
+            if (!token) return setError({ code: 401, message: "Token manquant." });
+    
+            const res = await callApi("/stats/user-stats/QperDay", token, "GET");
+            console.log(res.results)
+            setUserDate(res.results)
+        } catch (err) {
+            if (axios.isAxiosError(err)) {
+                setError({
+                    code: err.response?.status || 500,
+                    message: err.response?.data?.message || "Erreur lors du chargement des jeux.",
+                });
+            } else {
+                setError({ code: 500, message: "Erreur inconnue." });
+            }
+        }
+    };
+    
+    useEffect(() => {
+        fetchData()
+    }, []);
+
+    if (error.code)
+        return (
+            <div className="flex justify-center mt-8">
+                <Error number={error.code} message={error.message} />
+            </div>
+        );
     return (
         <Box sx={{ width: '100%', height: 300 }}>
             <LineChart
                 series={[
-                    { data: pData, label: 'pv' },
-                    { data: uData, label: 'uv' },
+                    { data: userData, label: 'Nombre Utilisateurs' }
                 ]}
-                xAxis={[{ scaleType: 'point', data: xLabels }]}
+                xAxis={[{ scaleType: 'point', data: Time }]}
                 yAxis={[{ width: 50 }]}
                 margin={margin}
             />
