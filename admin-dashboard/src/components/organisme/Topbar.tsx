0fdd0a67b2212cd532a63a12a3bccad941dcc924
Tidragon <theomoon77@gmail.com>
2025-11-25 00:42:24 +0100

    #045 / feat: Displaying the name in the header
    - TopBar

--- admin-dashboard/src/components/organisme/Topbar.tsx
+++ admin-dashboard/src/components/organisme/Topbar.tsx
@@ -1,11 +1,44 @@
-import  { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import callApi from "../../api/api";
+import type { ErrorState } from "../../types/error";
+
 
 export default function Topbar() {
-  const [token, setToken] = useState<string | null>(null);
+  const [error, setError] = useState<ErrorState>({ code: 0, message: "" });
+  const [loading, setLoading] = useState(true);
+  const [name, setName] = useState<string | null>(null);
+  const [role, setRole] = useState<string | null>(null);
+
+
+  const fetchData = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        setError({ code: 401, message: "Aucun token trouvé. Veuillez vous reconnecter." });
+        return;
+      }
+
+      const res = await callApi("/users/info/me", token, "GET");
+      setName(res.users.nickname);
+      setRole(res.users.role);
+    } catch (err: unknown) {
+      if (axios.isAxiosError(err)) {
+        setError({
+          code: err.response?.status || 500,
+          message: err.response?.data?.message || "Erreur lors du chargement des jeux.",
+        });
+      } else {
+        setError({ code: 500, message: "Erreur inconnue." });
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
 
   useEffect(() => {
-    const storedToken = localStorage.getItem("token");
-    setToken(storedToken);
+    fetchData();
   }, []);
 
   const handleLogout = () => {
@@ -13,26 +46,35 @@ export default function Topbar() {
     window.location.href = "/"; // redirige vers la page de login
   };
 
-  return (
-    <header className="flex justify-between items-center px-6 py-3 bg-white shadow-sm">
-      <h2 className="text-xl font-semibold text-[#0D0A4B]">Admin Dashboard</h2>
+  if (loading)
+    return <p className="text-center mt-8 text-gray-600">Chargement des jeux...</p>;
 
-      <div className="flex items-center gap-4">
-        {token ? (
-          <span className="text-sm text-gray-700 max-w-xs truncate">
-            Token: {token}
-          </span>
-        ) : (
-          <span className="text-sm text-gray-500">Aucun token</span>
-        )}
-
-        <button
-          onClick={handleLogout}
-          className="bg-[#4F7C77] text-white px-4 py-2 rounded-lg hover:opacity-80"
-        >
-          Logout
-        </button>
+  if (error.code)
+    return (
+      <div className="flex justify-center mt-8">
+        Error, name {error.code}: {error.message}
       </div>
-    </header>
-  );
+    );
+  return (
+  <header className="flex justify-between items-center px-6 py-3 bg-white shadow-sm">
+    <h2 className="text-xl font-semibold text-[#0D0A4B]">Admin Dashboard</h2>
+
+    <div className="flex items-center gap-4">
+      {name ? (
+        <span  className={`text-sm max-w-xs truncate ${role === "admin" ? "text-red-500 font-bold" : "text-gray-500"}`} >
+          Bienvenue, {name} ({role})
+        </span>
+      ) : (
+        <span className="text-sm text-gray-500">Aucun nom</span>
+      )}
+
+      <button
+        onClick={handleLogout}
+        className="bg-[#4F7C77] text-white px-4 py-2 rounded-lg hover:opacity-80"
+      >
+        Logout
+      </button>
+    </div>
+  </header>
+);
 }
